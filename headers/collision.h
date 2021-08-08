361fbc9eabe74349f13df9794d06a54df8c8391f
Eilif B <eilifb@gmail.com>
2021-08-08 03:13:07 +0200

    started work on Collision::checkCollision(Polygon a, Polygon b);

---
+++ headers/collision.h
@@ -0,0 +1,14 @@
+#ifndef COLLISION_H
+#define COLLISION_H
+
+class Vec;
+class Polygon;
+
+class Collision {
+public:
+	void checkCollision(Polygon a, Polygon b);
+};
+
+
+
+#endif // !COLLISION_H
---
+++ source/collision.cpp
@@ -0,0 +1,10 @@
+#include "collision.h"
+#include "shapes.h"
+
+
+void Collision::checkCollision(Polygon a, Polygon b){
+	std::vector<Vec> dotsA;
+	std::vector<Vec> dotsB;
+
+
+}
