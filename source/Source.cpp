2250c77fc2e80c0c6f6f44c99847251bdb59a8b7
Eilif B <eilifb@gmail.com>
2021-08-18 00:23:35 +0200

    did some minor cleanup

--- source/Source.cpp
+++ source/Source.cpp
@@ -9,32 +9,14 @@
 #include "test_suite.h"
 #include "utils.h"
 
-void test_vector() {
-	Vec a, b;
-	a.x = 3;
-	a.y = 4;
-	b.x = 3;
-	b.y = 1;
-
-	a = a + b;
-	a = -a;
-	std::cout << "vector: " << a;
-}
-
 
 int main(int argc, char* argv[]) {
 	printf("setting up\n");
-
-	Vec a(10, 10);
-	a.len();
-	a.len();
 	
 	test::test_collision();
 	test::test_collision_time();
-	//std::cout << -0.2f << " : " << std::hexfloat << -0.2f << std::endl;
-	double n = -0x1.9999800000000p-3;
 
 
-
+	std::cout << "Exiting..." << std::endl;
 	return 0;
 }
